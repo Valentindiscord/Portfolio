0245582747dd7ef0d1a585b61a441ae046be4a86
Valentin <53320666+Valentindiscord@users.noreply.github.com>
2024-10-02 13:22:05 +0530

    social links
    changed social links

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,12 +8,12 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/Valentindiscord/",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
+    "https://www.linkedin.com/in/pokajeevankumar/",
+  linkedin: "https://www.linkedin.com/in/pokajeevankumar/",
+  twitter: "https://twitter.com/JeevanKTweets",
+  insta: "https://www.instagram.com/",
   facebook: "https://www.facebook.com/rishav.chanda.165/",
 };
 
