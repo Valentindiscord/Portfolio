23d2b7eb223e9eb60ffca2ab536da6bd01021736
Valentin <53320666+Valentindiscord@users.noreply.github.com>
2024-10-02 22:52:21 +0530

    education
    education

--- src/data/constants.js
+++ src/data/constants.js
@@ -205,7 +205,7 @@ export const experiences = [
     role: "Administrator · Freelance",
     company: "PUBG Mobile | Tencent | Krafton",
     date: "Dec 2019 - Aug 2020",
-    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    desc: "Administrator at PUBG Mobile",
 
   },
   {
@@ -237,7 +237,7 @@ export const experiences = [
   },
   {
     id: 3,
-    img: "hhttps://media.licdn.com/dms/image/v2/D560BAQGJCu2U_L9CNQ/company-logo_100_100/company-logo_100_100/0/1725898995592/riot_games_logo?e=1735776000&v=beta&t=jxjIAr_zxrpdqlXTAv_ymwW-xj3Y7kP3-oSW_NeMK5o",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAGvRhdg1vaZyhkn5zzE7p35e70SUgv0TVCw&s",
     role: "Administrator",
     company: "Riot Games · Freelance",
     date: "Sep 2020 - Mar 2021",
@@ -300,30 +300,19 @@ export const experiences = [
 export const education = [
   {
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
-    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
-    date: "Oct 2021 - Sep 2025",
-    grade: "8.71 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
-    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    img: "https://media.licdn.com/dms/image/v2/C4D0BAQEFBRt0eM_tjA/company-logo_100_100/company-logo_100_100/0/1630507541099?e=1735776000&v=beta&t=v0WBiKz2B8chU1eu9aNUlKuv8xrlHl74LAsXkkeWChc",
+    school: "Hindu College, University of Delhi",
+    degree: "Master of Business Administration - MBA",
   },
   {
     id: 1,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkx821vptR6c2IxZ2MA68hZHikhuNeTtellg&s",
+    school: "Shishu Vikas High School",
     date: "Apr 2019 - Apr 2025",
     grade: "88.2%",
-    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
-    degree: "ISC(XII), Science with Computer",
-  },
-  {
-    id: 2,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2017 - Apr 2019",
-    grade: "92.3%",
-    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
-    degree: "ICSC(X), Science with Computer",
+    desc: "I completed my LKG to class 6 school education at Shishu Vikas High School.",
+    degree: "LKG to Sixth Standardr",
+
   },
 ];
 
